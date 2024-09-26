115e8bb3e6147a815e216999ffc31c4741b3dc98
seba.zldk <sebastian.garrido1@mail.udp.cl>
2024-09-26 17:07:06 -0300

    carrusel nuevo

--- portafolio/src/components/Carousel.tsx
+++ portafolio/src/components/Carousel.tsx
@@ -2,66 +2,58 @@ import "../styles/carousel.css";
 
 function Carousel() {
   return (
-    <div id="carousel" className="carousel slide" data-bs-ride="carousel">
-      <div className="carousel-inner">
-        <div className="carousel-item active" data-bs-interval="3000">
-          <div className="carousel-images">
-            <img src="/images/python.svg" className="d-block" alt="Python" />
-            <img src="/images/react.svg" className="d-block" alt="React" />
-            <img
-              src="/images/javascript.svg"
-              className="d-block"
-              alt="Javascript"
-            />
-            <img src="/images/html5.svg" className="d-block" alt="Html" />
-            <img src="/images/css.svg" className="d-block" alt="Css" />
-            <img src="/images/mysql.svg" className="d-block" alt="MySql" />
-            <img src="/images/django.svg" className="d-block" alt="Django" />
-            <img
-              src="/images/postgresql.svg"
-              className="d-block"
-              alt="Postgresql"
-            />
-          </div>
+    <div className="slider">
+      <div className="slide-track">
+        <div className="slide">
+          <img src="/images/python.svg" alt="" />
         </div>
-        <div className="carousel-item" data-bs-interval="3000">
-          <img src="/images/html5.svg" className="d-block" alt="Html" />
+        <div className="slide">
+          <img src="/images/react.svg" alt="" />
         </div>
-        <div className="carousel-item" data-bs-interval="3000">
-          <img src="/images/css.svg" className="d-block" alt="Css" />
+        <div className="slide">
+          <img src="/images/javascript.svg" alt="" />
         </div>
-        <div className="carousel-item" data-bs-interval="3000">
-          <img src="/images/mysql.svg" className="d-block" alt="MySql" />
+        <div className="slide">
+          <img src="/images/html5.svg" alt="" />
         </div>
-        <div className="carousel-item" data-bs-interval="3000">
-          <img
-            src="/images/postgresql.svg"
-            className="d-block"
-            alt="Postgresql"
-          />
+        <div className="slide">
+          <img src="/images/css.svg" alt="" />
         </div>
-        <div className="carousel-item" data-bs-interval="3000">
-          <img src="/images/django.svg" className="d-block" alt="Django" />
+        <div className="slide">
+          <img src="/images/mysql.svg" alt="" />
+        </div>
+        <div className="slide">
+          <img src="/images/django.svg" alt="" />
+        </div>
+        <div className="slide">
+          <img src="/images/postgresql.svg" alt="" />
+        </div>
+
+        <div className="slide">
+          <img src="/images/python.svg" alt="" />
+        </div>
+        <div className="slide">
+          <img src="/images/react.svg" alt="" />
+        </div>
+        <div className="slide">
+          <img src="/images/javascript.svg" alt="" />
+        </div>
+        <div className="slide">
+          <img src="/images/html5.svg" alt="" />
+        </div>
+        <div className="slide">
+          <img src="/images/css.svg" alt="" />
+        </div>
+        <div className="slide">
+          <img src="/images/mysql.svg" alt="" />
+        </div>
+        <div className="slide">
+          <img src="/images/django.svg" alt="" />
+        </div>
+        <div className="slide">
+          <img src="/images/postgresql.svg" alt="" />
         </div>
       </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleInterval"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleInterval"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
     </div>
   );
 }
--- portafolio/src/components/Proyects.tsx
+++ portafolio/src/components/Proyects.tsx
@@ -6,30 +6,117 @@ function Proyect() {
       <h1 className="titulo">Trabajos.</h1>
       <div className="div-proyect">
         <h2 className="proyects">App Movil</h2>
-        <button className="preview" type="button">
-          DEMO
-        </button>
-        <button className="code" type="button">
-          CODE
-        </button>
+        <div className="image-container">
+          <img
+            src="../../public/images/qml.svg"
+            className="d-block"
+            alt="Qml"
+          />
+          <img
+            src="../../public/images/sqlite.svg"
+            className="d-block"
+            alt="Sqlite"
+          />
+          <img
+            src="../../public/images/python2.svg"
+            className="d-block"
+            alt="Javascript"
+          />
+          <img
+            src="../../public/images/css2.svg"
+            className="d-block"
+            alt="Css"
+          />
+          <img
+            src="../../public/images/html5.svg"
+            className="d-block"
+            alt="Html"
+          />
+        </div>
+        <div className="buttons-container">
+          <button className="preview" type="button">
+            DEMO
+          </button>
+          <button className="code" type="button">
+            CODE
+          </button>
+        </div>
       </div>
       <div className="div-proyect">
         <h2 className="proyects">App movil 2</h2>
-        <button className="preview" type="button">
-          DEMO
-        </button>
-        <button className="code" type="button">
-          CODE
-        </button>
+        <div className="image-container">
+          <img
+            src="../../public/images/qml.svg"
+            className="d-block"
+            alt="Qml"
+          />
+          <img
+            src="../../public/images/sqlite.svg"
+            className="d-block"
+            alt="Sqlite"
+          />
+          <img
+            src="../../public/images/python2.svg"
+            className="d-block"
+            alt="Javascript"
+          />
+          <img
+            src="../../public/images/css2.svg"
+            className="d-block"
+            alt="Css"
+          />
+          <img
+            src="../../public/images/html5.svg"
+            className="d-block"
+            alt="Html"
+          />
+        </div>
+        <div className="buttons-container">
+          <button className="preview" type="button">
+            DEMO
+          </button>
+          <button className="code" type="button">
+            CODE
+          </button>
+        </div>
       </div>
       <div className="div-proyect">
         <h2 className="proyects">App Web</h2>
-        <button className="preview" type="button">
-          DEMO
-        </button>
-        <button className="code" type="button">
-          CODE
-        </button>
+        <div className="image-container">
+          <img
+            src="../../public/images/qml.svg"
+            className="d-block"
+            alt="Qml"
+          />
+          <img
+            src="../../public/images/sqlite.svg"
+            className="d-block"
+            alt="Sqlite"
+          />
+          <img
+            src="../../public/images/python2.svg"
+            className="d-block"
+            alt="Javascript"
+          />
+          <img
+            src="../../public/images/css2.svg"
+            className="d-block"
+            alt="Css"
+          />
+          <img
+            src="../../public/images/html5.svg"
+            className="d-block"
+            alt="Html"
+          />
+        </div>
+        <div className="buttons-container">
+          <button className="preview" type="button">
+            DEMO
+          </button>
+          <button className="code" type="button">
+            CODE
+          </button>
+        </div>
       </div>
     </div>
   );
